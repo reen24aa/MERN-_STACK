fc6eff75d7e3b6471dd6ff2497c3d6f9fe10eb1b
reen24aa <122306516+reen24aa@users.noreply.github.com>
2024-08-22 18:53:05 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,2 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
-const auth = getAuth(app);
-
-export { auth };
 
